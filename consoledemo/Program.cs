bb95fa970f850b34e6f08614b8ede4df92a046b1
Boots <githubBoots@bloomski.com>
2025-02-25 15:29:17 +0800

    第四次提交 - 午安 , 你好嗎!

--- consoledemo/Program.cs
+++ consoledemo/Program.cs
@@ -3,7 +3,9 @@
     private static void Main(string[] args)
     {
         //Console.WriteLine("Hello, World!");
-        Console.WriteLine("早安 , 你好嗎!");
+        //Console.WriteLine("早安 , 你好嗎!");
+        Console.WriteLine("午安 , 你好嗎!");
+
         //Console.Readkey();
     }
 }
