89bb870b897c75de4cb7c5013af2b9d6e9c28918
Boots <githubBoots@bloomski.com>
2025-02-25 15:32:11 +0800

    第七次提交 - 晚安(09:00) , 你好嗎!

--- consoledemo/Program.cs
+++ consoledemo/Program.cs
@@ -6,7 +6,8 @@
         //Console.WriteLine("早安 , 你好嗎!");
         //Console.WriteLine("午安 , 你好嗎!");
         //Console.WriteLine("晚安 , 你好嗎!");
-        Console.WriteLine("晚安(08:00) , 你好嗎!");
+        //Console.WriteLine("晚安(08:00) , 你好嗎!");
+        Console.WriteLine("晚安(09:00) , 你好嗎!");
 
 
         Console.Readkey();
