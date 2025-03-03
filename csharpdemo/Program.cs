fb7e5eb0627fe85eb2cbe7eaa21a8a58c80c33c5
Boots <githubBoots@bloomski.com>
2025-03-03 08:45:23 +0800

    114-03-03 update from TC. Starting C#

---
+++ csharpdemo/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static double length =0;
+    static double width =0;
+    private static void Main(string[] args)
+    {
+        Config();
+        Display();
+        Console.WriteLine("Enter any key to stop.");
+        Console.ReadKey();
+
+    }
+
+    static void Config(){
+        length = 4.0;
+        width = 3.0;
+    }
+
+    static void Display(){
+        Console.WriteLine("Length: {0}", length);
+        Console.WriteLine("Width: {0}", width);
+        Console.WriteLine("Area: {0}", GetArea());
+    }
+
+    static double GetArea(){
+        return length * width;
+    }
+}
