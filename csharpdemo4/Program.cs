9c9ac6f6de348fdea57d1ce0010bd37998c14712
Boots <githubBoots@bloomski.com>
2025-03-26 11:59:59 +0800

    練習C#網頁

---
+++ csharpdemo4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        demo();
+       // Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+    static void demo()
+    {
+        // 顯示標題列
+        string str_title = "螢幕顯示出 Hello World!!";
+
+        int[] numberArray1;
+        int[] numberArray2 = new int[5];
+        int[] intrArray1 = {1, 2, 3};
+        
+    }
+}
---
+++ mvcdemo/Controllers/RazorDemoController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class RazorDemoController : Controller
+    {
+        public IActionResult Demo01()
+        {
+            return View();
+        }
+
+        public IActionResult Demo02()
+        {
+            return View();
+        }
+        public IActionResult Demo03()
+        {
+            return View();
+        }
+        public IActionResult Demo04()
+        {
+            return View();
+        }
+        public IActionResult Demo05()
+        {
+            return View();
+        }
+        public IActionResult Demo06()
+        {
+            return View();
+        }
+        public IActionResult Demo07()
+        {
+            return View();
+        }
+        public IActionResult Demo08()
+        {
+            return View();
+        }
+        public IActionResult Demo09()
+        {
+            return View();
+        }
+        public IActionResult Demo10()
+        {
+            return View();
+        }
+        public IActionResult Demo11()
+        {
+            return View();
+        }
+        public IActionResult Demo12()
+        {
+            return View();
+        }
+        public IActionResult Demo13()
+        {
+            return View();
+        }
+        public IActionResult Demo14()
+        {
+            return View();
+        }
+        public IActionResult Demo15()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+    }
+}
